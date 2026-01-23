44f54a3328bec9dc3b5a001729fc03902836f0f9
Zelia Sequeira <zsequeira9@gmail.com>
2026-01-23 10:47:40 -0500

    audio analyser class

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import AudioAnalyser from './AudioAnalyser';
 
 const width = 30
 const height = 30 
@@ -6,37 +7,24 @@ const height = 30
 // create ThreeJS scene
 const scene = new THREE.Scene();
 const camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+camera.position.z = 10;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight - 50 );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
-const cubes = [] as THREE.Mesh[]
 
-// create cube
+// create cube geometry
+const cubes = [] as THREE.Mesh[]
 const geometry = new THREE.BoxGeometry( .1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-
-camera.position.z = 10;
-
-
 // create audio analyser node
-const audioCtx = new AudioContext();
-const analyser = audioCtx.createAnalyser();
-let source;
-let buffer: AudioBuffer;
-const fftSize = 256;
+const audioAnalyser = new AudioAnalyser();
 
-analyser.fftSize = fftSize;
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-analyser.getByteFrequencyData(dataArray);
-
+// set up vis
 const numCubes = 128;
-
-const cubefreq = fftSize / numCubes
-
+const sizeBin = audioAnalyser.fftSize / numCubes
 for (let i = 0; i < numCubes; i++) {
   const cube = new THREE.Mesh( geometry, material );
   cube.position.x = i * .2 - 14
@@ -45,13 +33,13 @@ for (let i = 0; i < numCubes; i++) {
 }
 
 /**
- * Animate the cube based on waveform data
+ * Animate the cubes based on waveform data
  */
 function animate() {
-    analyser.getByteFrequencyData(dataArray);
+    const freqData = audioAnalyser.frequencyData;
     for (let i = 0; i < cubes.length; i++) {
-      const avg = dataArray.slice(i*cubefreq, i*cubefreq+cubefreq).reduce(
-        (acc, curr) => acc + curr, 0) / cubefreq
+      const avg = freqData.slice(i*sizeBin, i*sizeBin+sizeBin).reduce(
+        (acc, curr) => acc + curr, 0) / sizeBin
       cubes[i].scale.y = avg / 10;
       cubes[i].position.y = -13 + avg / 20;
     }
@@ -61,35 +49,19 @@ function animate() {
 
 
 /**
- * Load the audio from filesystem
- */
-function load() {
-  const request = new XMLHttpRequest();
-  request.open("GET", "/test4.flac");
-  request.responseType = "arraybuffer";
-  request.onload = function() {
-    let undecodedAudio = request.response;
-    audioCtx.decodeAudioData(undecodedAudio, (data) => buffer = data); 
-  };
-  request.send();
-}
-
-
-/**
- * Play the audio source through destination
- *  and analyser
+ * Load and play audio from filesystem
  */
 function play() {
-    source = audioCtx.createBufferSource();
-    source.buffer = buffer;
-    source.connect(analyser);
-    source.connect(audioCtx.destination);
-    source.start();   
+    const request = new XMLHttpRequest();
+    request.open("GET", "/test4.flac");
+    request.responseType = "arraybuffer";
+    request.onload = function() {
+      const undecodedAudio = request.response;
+      audioAnalyser.play(undecodedAudio);      
+    };
+  request.send();
+  
 }
 
-// UI
-let button1 = document.getElementById("load")!;
-button1.onclick = load;
-
-let button2 = document.getElementById("play")!;
-button2.onclick = play;
+let playButton = document.getElementById("play")!;
+playButton.onclick = play;
