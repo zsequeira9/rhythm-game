b5316f4fb0771691c9d8ff9ac339a9c358d2ad47
Zelia Sequeira <zsequeira9@gmail.com>
2026-02-16 20:51:26 -0500

    aaaaaaaa

--- src/audio-analyser.ts
+++ src/audio-analyser.ts
@@ -16,10 +16,10 @@ export default class AudioAnalyser {
     this.processorOutput = this.audioCtx.createAnalyser();
     this.fftSize = fftSize
     this.analyser.fftSize = this.fftSize;
-    this.processorOutput.fftSize = 8192;
+    this.processorOutput.fftSize = this.fftSize;
     this.bufferLength = this.analyser.frequencyBinCount;
     this.dataArray = new Uint8Array(this.bufferLength)
-    this.isBeat = new Float32Array(8192)
+    this.isBeat = new Float32Array(this.fftSize)
   }
 
   get frequencyData() {
@@ -29,7 +29,7 @@ export default class AudioAnalyser {
 
     get onsetDetection() {
       this.processorOutput.getFloatTimeDomainData(this.isBeat)
-      console.log(this.isBeat)
+      console.log(this.isBeat[0] >= .5)
       return this.isBeat[0] >= .5
     }
 
--- src/audio-processor.ts
+++ src/audio-processor.ts
@@ -1,38 +1,71 @@
 import { EssentiaWASM } from "https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia-wasm.es.js?url";
 import Essentia from "https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia.js-core.es.js?url";
 
-let essentia = new Essentia(EssentiaWASM);
-
 class AudioProcessor extends AudioWorkletProcessor {
   essentia: Essentia;
   lastOnset = false;
+  frames: { array: Float32Array<ArrayBuffer>; offset: number}[] = [];
+  detectedOnsets: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   constructor() {
     super();
-    this.essentia = essentia;
+    this.essentia = new Essentia(EssentiaWASM);
+    this.initFrames();
     console.log('Backend - essentia:' + this.essentia.version + '- http://essentia.upf.edu');
+
+  }
+
+  initFrames() {
+    for (let i = 0; i < 16; i++) {
+      this.frames[i] = {
+        array: new Float32Array(2048),
+        offset: -i - 1,
+      }
+    }
   }
 
   //System-invoked process callback function.
   process(inputs, outputs, parameters) {
 
     // <inputs> and <outputs> will have as many as were specified in the options passed to the AudioWorkletNode constructor, each subsequently spanning potentially multiple channels
-    let input = inputs[0];
+    let input = inputs[0][0];
     let output = outputs[0];
 
-    // convert the input audio frame array from channel 0 to a std::vector<float> type for using it in essentia
-    let vectorInput = this.essentia.arrayToVector(input[0]);
-    let spectrum = this.essentia.Spectrum(vectorInput).spectrum
+    this.frames.forEach(frame => {
+      frame.offset += 1
 
-    let phase = this.essentia.arrayToVector([])
+      if (frame.offset >= 0) {
+        frame.offset %= 16
+        frame.array.set(input, frame.offset * 128)
+    
+        if (frame.offset == 15) {
+          // start processing chain
 
-    let threshold = 0;
+          const signal = this.essentia.arrayToVector(frame.array)
+          // const eqloud = this.essentia.EqualLoudness(signal).signal
+          const window = this.essentia.Windowing(
+            signal, true, 2048, 'hann', 4, true
+          ).frame
 
-    let onsetDetection = this.essentia.OnsetDetection(spectrum, phase).onsetDetection // input audio frame
-    let tmpOnset = onsetDetection > threshold
-    let onset = !this.lastOnset && tmpOnset
-    this.lastOnset = tmpOnset
+          let spectrum = this.essentia.Spectrum(window).spectrum
 
-    output[0][0] = onset;
+          let phase = this.essentia.arrayToVector([])
+
+          let onsetDetection = this.essentia.OnsetDetection(spectrum, phase).onsetDetection
+
+          this.detectedOnsets.pop()
+
+          this.detectedOnsets.splice(0, 0, onsetDetection)
+
+          let alpha = .2
+
+          let threshold = this.detectedOnsets.toSorted()[2] + alpha * this.detectedOnsets.reduce((acc, curr) => acc + curr, 0) / this.detectedOnsets.length
+
+          let tmpOnset = onsetDetection > threshold
+          let onset = !this.lastOnset && tmpOnset
+          this.lastOnset = tmpOnset
+
+          output[0][0] = onset;
+    }}})
 
     return true; // keep the process running
   }
