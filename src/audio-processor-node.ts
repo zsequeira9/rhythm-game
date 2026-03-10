6f5acc0a1aee1f25912f251ff0ff428300454b08
Zelia Sequeira <zsequeira9@gmail.com>
2026-03-10 17:16:22 -0400

    check

--- src/audio-processor-node.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore 2307 - We must load from a url in a audio worklet
-import testProcessorUrl from "./audio-processor.ts?url";
-
- export async function createAudioProcessor(audioCtx: AudioContext) {
-    await audioCtx.audioWorklet.addModule(testProcessorUrl)
-    console.log("Creating Audio Worklet Node")
-    return new AudioWorkletNode(audioCtx, "audio-processor", {outputChannelCount: [1]});
- }
